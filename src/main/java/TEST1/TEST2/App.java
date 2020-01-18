9c04ddc21dbf67300d0df9d0001c6dbc138fdc48
senthilkumarselenium123 <senthilkumarselenium123@gmail.com>
2020-01-19 00:38:24 +0530

    first project

---
+++ src/main/java/TEST1/TEST2/App.java
@@ -0,0 +1,13 @@
+package TEST1.TEST2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/TEST1/TEST2/TESTTESTNG.java
@@ -0,0 +1,16 @@
+package TEST1.TEST2;
+
+import org.testng.annotations.Test;
+
+public class TESTTESTNG {
+
+	@Test
+	public static void abc() {
+		System.out.println("HGFHJHKLJLK");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
